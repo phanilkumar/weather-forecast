b218d140b9aed7f62aaf446483db6b8a43c1c5f7
phanil kumar <phanilkumar@outlook.com>
2025-10-08 15:52:33 +0530

    test: split success/failure cassettes; use ClimateControl for 401

--- spec/services/weather_providers/open_weather_map_client_spec.rb
+++ spec/services/weather_providers/open_weather_map_client_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe WeatherProviders::OpenWeatherMapClient do
-  it 'fetches current and forecast with Faraday' do
-    stub = Faraday.new do |b|
-      b.adapter :test do |stubs|
-        stubs.get('https://api.openweathermap.org/data/2.5/weather') { |env| [ 200, {}, { main: { temp: 72, feels_like: 70 }, weather: [ { main: 'Clear', description: 'clear sky' } ], wind: { speed: 3 } }.to_json ] }
-        stubs.get('https://api.openweathermap.org/data/2.5/forecast') { |env| [ 200, {}, { list: [] }.to_json ] }
+  it 'records and replays success', :vcr do
+    VCR.use_cassette('owm/success_current_and_forecast') do
+      client = described_class.new
+      data = client.fetch_current_and_5day(lat: 37.422, lon: -122.084)
+      expect(data[:current]).to be_a(Hash)
+      expect(data[:forecast]).to be_a(Hash)
+    end
+  end
+
+  it 'records and replays failure (401)', :vcr do
+    VCR.use_cassette('owm/failure_unauthorized') do
+      # Temporarily force a bad key for this cassette only
+      ClimateControl.modify OWM_API_KEY: 'bad_key' do
+        client = described_class.new
+        expect {
+          client.fetch_current_and_5day(lat: 37.422, lon: -122.084)
+        }.to raise_error(WeatherProviders::OpenWeatherMapClient::Unauthorized, /401/)
       end
     end
-
-    client = described_class.new(http_client: stub)
-    data = client.fetch_current_and_5day(lat: 1.0, lon: 2.0)
-    expect(data[:current]).to be_a(Hash)
-    expect(data[:forecast]).to be_a(Hash)
   end
 end
